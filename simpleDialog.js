c995e82f2765c124a7b56dc868b7e3de6d93d185
Ovaqlab <33068920+ovaqlab@users.noreply.github.com>
2018-01-29 16:39:18 +0530

    Empty callback option bug fixed
    Options without callback produce errors.

--- simpleDialog.js
+++ simpleDialog.js
@@ -123,6 +123,92 @@ $.simpleDialog = function (options, onSuccess, onClose) {
 
         /**
          * Validating options in simpleDaialog
+$.simpleDialog = function (options, onSuccess, onClose) {
+        var template = null;
+
+        function valdateOptions(success) {
+            if ((options.title || options.message) && options.modalContent) {
+                console.error("simpleDialog only accepts either modal content or title and message");
+                return;
+            }
+            else {
+                success();
+            }
+        }
+
+
+        /**
+         * Initializing modal with modal options
+         */
+        function init() {
+            if (options.modalContent) {
+                options.modalContent = options.modalContent.strReplace({
+                    title: options.title,
+                    message: options.message
+                });
+            } else {
+                console.error("Empty modal content. simpleDialog only accepts either modal content or title and message");
+                return;
+            }
+
+            if (!options.closeButton)
+                options.closeButtonTemplate = '';
+            else {
+                options.closeButtonTemplate = options.closeButtonTemplate.strReplace({
+                    closeBtnText: options.closeBtnText
+                });
+            }
+
+            template = '<div class="modal fade window-t-xs-40" id="simple-dialog-modal" tabindex="-1" role="dialog" data-backdrop="' + options.backdrop + '"' +
+                '           aria-labelledby="eventDetailsModal">' +
+                '           <div class="modal-dialog" role="document">' +
+                '               <div class="modal-content">' + options.modalContent +
+                '                   <div class="modal-footer no-border">' + options.closeButtonTemplate +
+                '                       <button type="button" id="confirm-btn" class=" btn btn-primary">' + options.confirmBtnText + '</button>' +
+                '                   </div>' +
+                '               </div>' +
+                '           </div>' +
+                '       </div>';
+            if (!$('body').hasClass('model-open')) {
+                $('body').append(template);
+                $('#simple-dialog-modal').modal('show');
+            }
+        }
+
+        /**
+         * Handling confirm button click event. If callback is available perform callback operation
+         */
+        $(document).on('click',"#confirm-btn", function (event) {
+            event.preventDefault();
+            $('#simple-dialog-modal').modal('hide');
+            if (typeof options.onSuccess === 'function' && options.onSuccess()) {
+                options.onSuccess();
+            }
+        });
+
+        /**
+         * Handling close button events
+         */
+        $(document).on('click',"#cancel-btn", function (event) {
+            event.preventDefault();
+            $('#simple-dialog-modal').modal('hide');
+            if (typeof options.onCancel === 'function' && options.onCancel())
+                options.onCancel();
+        });
+
+        /**
+         * removing appended modal from body and reset callback to null when modal become hidden
+         */
+        $(document).on('hidden.bs.modal', '#simple-dialog-modal', function () {
+            if(options.onSuccess)
+            options.onSuccess = null;
+            if(options.onCancel)
+            options.onCancel = null;
+            return $('#simple-dialog-modal').remove();
+        });
+
+        /**
+         * Validating options in simpleDaialog
          */
         valdateOptions(function () {
             /**

5ba49fc9111c1c55e9d441ef5f017e0ab359a11f
Ovaqlab <33068920+ovaqlab@users.noreply.github.com>
2018-05-04 15:42:00 +0530

    Bugs in close button and confirm button fixed
    Some minor bugs fixed. Button text not appearing in some cases fixed.

--- simpleDialog.js
+++ simpleDialog.js
@@ -27,212 +27,117 @@
 
 ;(function ($) {
 $.simpleDialog = function (options, onSuccess, onClose) {
-        var template = null;
+    var template = null;
 
-        function valdateOptions(success) {
-            if ((options.title || options.message) && options.modalContent) {
-                console.error("simpleDialog only accepts either modal content or title and message");
-                return;
-            }
-            else {
-                success();
-            }
-        }
+    function valdateOptions(success) {
+      if ((options.title || options.message) && options.modalContent) {
+        console.error("simpleDialog only accepts either modal content or title and message");
+        return;
+      } else {
+        success();
+      }
+    }
 
 
-        /**
-         * Initializing modal with modal options
-         */
-        function init() {
-            if (options.modalContent) {
-                options.modalContent = options.modalContent.strReplace({
-                    title: options.title,
-                    message: options.message
-                });
-            } else {
-                console.error("Empty modal content. simpleDialog only accepts either modal content or title and message");
-                return;
-            }
-
-            if (!options.closeButton)
-                options.closeButtonTemplate = '';
-            else {
-                options.closeButtonTemplate = options.closeButtonTemplate.strReplace({
-                    closeBtnText: options.closeBtnText
-                });
-            }
-
-            template = '<div class="modal fade window-t-xs-40" id="simple-dialog-modal" tabindex="-1" role="dialog" data-backdrop="' + options.backdrop + '"' +
-                '           aria-labelledby="eventDetailsModal">' +
-                '           <div class="modal-dialog" role="document">' +
-                '               <div class="modal-content">' + options.modalContent +
-                '                   <div class="modal-footer no-border">' + options.closeButtonTemplate +
-                '                       <button type="button" id="confirm-btn" class=" btn btn-primary">' + options.confirmBtnText + '</button>' +
-                '                   </div>' +
-                '               </div>' +
-                '           </div>' +
-                '       </div>';
-            if (!$('body').hasClass('model-open')) {
-                $('body').append(template);
-                $('#simple-dialog-modal').modal('show');
-            }
-        }
-
-        /**
-         * This function helps to prevent errors like "modal is transitioning"
-         */
-        function hideModal() {
-            var modal = $("#simple-dialog-modal");
-            var bsModal = modal.data('bs.modal');
-            bsModal["_isShown"] = false;
-            bsModal["_isTransitioning"] = false;
-            modal.data('bs.modal', bsModal);
-        }
-
-        /**
-         * Handling confirm button click event. If callback is available perform callback operation
-         */
-        $(document).on('click', "#confirm-btn", function (event) {
-            event.preventDefault();
-            $('#simple-dialog-modal').modal('hide');
-            hideModal();
-            if (typeof options.onSuccess === 'function' && options.onSuccess()) {
-                options.onSuccess();
-            }
+    /**
+     * Initializing modal with modal options
+     */
+    function init() {
+      if (options.modalContent) {
+        options.modalContent = options.modalContent.strReplace({
+          title: options.title,
+          message: options.message
         });
+      } else {
+        console.error("Empty modal content. simpleDialog only accepts either modal content or title and message");
+        return;
+      }
 
-        /**
-         * Handling close button events
-         */
-        $(document).on('click', "#cancel-btn", function (event) {
-            event.preventDefault();
-            $('#simple-dialog-modal').modal('hide');
-            hideModal();
-            if (typeof options.onCancel === 'function' && options.onCancel())
-                options.onCancel();
+      if (!options.closeButton)
+        options.closeButtonTemplate = '';
+      else {
+        options.closeButtonTemplate = options.closeButtonTemplate.strReplace({
+          closeBtnText: options.closeBtnText,
+          closeBtnClass: options.closeBtnClass
         });
+      }
 
-        /**
-         * removing appended modal from body and reset callback to null when modal become hidden
-         */
-        $(document).on('hidden.bs.modal', '#simple-dialog-modal', function () {
-            options.onSuccess = null;
-            options.onCancel = null;
-            return $('#simple-dialog-modal').remove();
-        });
+      template = '<div class="modal fade window-t-xs-40" id="simple-dialog-modal" tabindex="-1" role="dialog" data-backdrop="' + options.backdrop + '"' +
+        '           aria-labelledby="eventDetailsModal">' +
+        '           <div class="modal-dialog" role="document">' +
+        '               <div class="modal-content">' + options.modalContent +
+        '                   <div class="modal-footer no-border">' + options.closeButtonTemplate +
+        '                       <button type="button" id="confirm-btn" class=" btn ' + options.confirmBtnClass + '">' + options.confirmBtnText + '</button>' +
+        '                   </div>' +
+        '               </div>' +
+        '           </div>' +
+        '       </div>';
+      if (!$('body').hasClass('model-open')) {
+        $('body').append(template);
+        $('#simple-dialog-modal').modal('show');
+      }
+    }
 
-        /**
-         * Validating options in simpleDaialog
-$.simpleDialog = function (options, onSuccess, onClose) {
-        var template = null;
+    /**
+     * Handling confirm button click event. If callback is available perform callback operation
+     */
+    $(document).on('click', "#confirm-btn", function (event) {
+      event.preventDefault();
+      $('#simple-dialog-modal').modal('hide');
+      if (typeof options.onSuccess === 'function' && options.onSuccess()) {
+        options.onSuccess();
+      }
+    });
 
-        function valdateOptions(success) {
-            if ((options.title || options.message) && options.modalContent) {
-                console.error("simpleDialog only accepts either modal content or title and message");
-                return;
-            }
-            else {
-                success();
-            }
-        }
+    /**
+     * Handling close button events
+     */
+    $(document).on('click', "#cancel-btn", function (event) {
+      event.preventDefault();
+      $('#simple-dialog-modal').modal('hide');
+      if (typeof options.onCancel === 'function' && options.onCancel())
+        options.onCancel();
+    });
 
+    /**
+     * removing appended modal from body and reset callback to null when modal become hidden
+     */
+    $(document).on('hidden.bs.modal', '#simple-dialog-modal', function () {
+      if (options.onSuccess)
+        options.onSuccess = null;
+      if (options.onCancel)
+        options.onCancel = null;
+      return $('#simple-dialog-modal').remove();
+    });
 
-        /**
-         * Initializing modal with modal options
-         */
-        function init() {
-            if (options.modalContent) {
-                options.modalContent = options.modalContent.strReplace({
-                    title: options.title,
-                    message: options.message
-                });
-            } else {
-                console.error("Empty modal content. simpleDialog only accepts either modal content or title and message");
-                return;
-            }
-
-            if (!options.closeButton)
-                options.closeButtonTemplate = '';
-            else {
-                options.closeButtonTemplate = options.closeButtonTemplate.strReplace({
-                    closeBtnText: options.closeBtnText
-                });
-            }
-
-            template = '<div class="modal fade window-t-xs-40" id="simple-dialog-modal" tabindex="-1" role="dialog" data-backdrop="' + options.backdrop + '"' +
-                '           aria-labelledby="eventDetailsModal">' +
-                '           <div class="modal-dialog" role="document">' +
-                '               <div class="modal-content">' + options.modalContent +
-                '                   <div class="modal-footer no-border">' + options.closeButtonTemplate +
-                '                       <button type="button" id="confirm-btn" class=" btn btn-primary">' + options.confirmBtnText + '</button>' +
-                '                   </div>' +
-                '               </div>' +
-                '           </div>' +
-                '       </div>';
-            if (!$('body').hasClass('model-open')) {
-                $('body').append(template);
-                $('#simple-dialog-modal').modal('show');
-            }
-        }
-
-        /**
-         * Handling confirm button click event. If callback is available perform callback operation
-         */
-        $(document).on('click',"#confirm-btn", function (event) {
-            event.preventDefault();
-            $('#simple-dialog-modal').modal('hide');
-            if (typeof options.onSuccess === 'function' && options.onSuccess()) {
-                options.onSuccess();
-            }
-        });
-
-        /**
-         * Handling close button events
-         */
-        $(document).on('click',"#cancel-btn", function (event) {
-            event.preventDefault();
-            $('#simple-dialog-modal').modal('hide');
-            if (typeof options.onCancel === 'function' && options.onCancel())
-                options.onCancel();
-        });
-
-        /**
-         * removing appended modal from body and reset callback to null when modal become hidden
-         */
-        $(document).on('hidden.bs.modal', '#simple-dialog-modal', function () {
-            if(options.onSuccess)
-            options.onSuccess = null;
-            if(options.onCancel)
-            options.onCancel = null;
-            return $('#simple-dialog-modal').remove();
-        });
-
-        /**
-         * Validating options in simpleDaialog
-         */
-        valdateOptions(function () {
-            /**
-             * Default options
-             */
-            options = $.extend({
-                title: 'Confirm',
-                message: 'Do you want to continue?',
-                modalContent: '<div class="modal-header bg-white">' +
-                '                  <h4 class="modal-title capitalize-first-letter" id="exampleModalLabel">{title}</h4>' +
-                '               </div>' +
-                '               <div class="modal-body ">{message}' +
-                '               </div>',
-                confirmBtnText: 'Okay',
-                closeBtnText: 'close',
-                backdrop: true,
-                closeButton: true,
-                closeButtonTemplate: '<button type="button" id ="cancel-btn" class="btn btn-default">{closeBtnText}</button>',
-                onSuccess: null,
-                onCancel: null
-            }, options);
-            init();
-        });
-    };
+    /**
+     * Validating options in simpleDaialog
+     */
+    valdateOptions(function () {
+      /**
+       * Default options
+       */
+      options = $.extend({
+        title: 'Confirm',
+        message: 'Do you want to continue?',
+        modalContent: '<div class="modal-header bg-white">' +
+          '                  <h4 class="modal-title capitalize-first-letter" id="exampleModalLabel">{title}</h4>' +
+          '               </div>' +
+          '               <div class="modal-body ">{message}' +
+          '               </div>',
+        confirmBtnText: 'Okay',
+        closeBtnText: 'close',
+        confirmBtnClass: 'btn-primary',
+        closeBtnClass: 'btn-default',
+        backdrop: true,
+        closeButton: true,
+        closeButtonTemplate: '<button type="button" id ="cancel-btn" class="btn {closeBtnClass}">{closeBtnText}</button>',
+        onSuccess: null,
+        onCancel: null
+      }, options);
+      init();
+    });
+  };
 
     /**
      * String replace function to replace variables with values in a string.
